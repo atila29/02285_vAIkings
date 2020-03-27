791be99b14268a664a4b3f70ae272cdfd6cadf19
mariagrohshennig <mariatgrohs@gmail.com>
2020-03-27 14:51:14 +0100

    Merge branch 'master' of /Users/mariagrohshennig/Desktop/DTU/02285 AI & MAS/02285_vAIkings with conflicts.

--- client/agent.py
+++ client/agent.py
@@ -1,18 +1,19 @@
 from action import ActionType, ALL_ACTIONS
-from state import State
 from level import AgentElement
 from box import Box
+from state import State
 
 class Agent:
+    color: str  # maybe enums?
+    name: str
+    row: int
+    col: int
 
-    def __init__(self, row, col, color, id):
+    def __init__(self, id, color, row, col):
         self.row = row
         self.col = col
         self.color = color
         self.id = id
-
-    def __str__(self):
-        return "Logical agent with ID " + id
 
     def get_children(self, current_state):
         children = []
@@ -67,3 +68,98 @@ class Agent:
 
         #Shuffle children
         return children
+
+    def __repr__(self):
+        return self.color + " Agent with letter " + self.name
+
+    def __str__(self):
+        return self.name
+
+
+class BDIAgent(Agent):
+
+    def __init__(self,
+            id,          
+            color,
+            row,
+            col, 
+            initial_beliefs, 
+            initial_intentions):
+            
+        super().__init__(id, color, row, col)
+        self.beliefs = initial_beliefs
+        self.intentions = initial_intentions
+        # self.path = [] #tar vare på veien agenten beveger seg
+
+    def get_next_percept(self):
+        return
+
+    def brf(self, p):  # belief revision function, return new beliefs (updated in while-loop)
+        return
+
+    def deliberate(self):  # returns intentions (computed in filter())
+        return filter()
+
+    def options(self):  # used in deliberate
+        return
+
+    def filter(self):  # used in deliberate
+        return
+
+    def sound(self, plan):  # returns true/false, if sound return true
+        return
+
+    def suceeded(self):
+        return
+
+    def impossible(self):
+        return
+
+    def reconsider(self):
+        return
+
+    def execute(self, a): #execute an action
+        #update internal knowledge of you position, the state is updated from the client
+        return
+
+    def extract_plan(self, current_state) -> '[Action,...]': #include this function?
+        
+        """
+            E.g. Version 2 from slide 24 week 5
+                1. & 2. removed since loop is in client instead
+                3. get next percept ρ, in our case the current state;
+                4. Update beliefs, B := brf (B,ρ);
+                5. Update Intentions, I := deliberate(B);
+                6. Make plan, π:= plan(B,I);
+                7. Return plan π
+        Q: Do we need to save in new variables 
+        until we know from server that the plan is OK?
+        Q: In case of conflict we might need to make some agents replan,
+        How do we get them to choose another plan?
+            Idea 1: Give them a number i, 0 as default, to tell them to choose the
+            'ith best' plan.
+        """
+        pass
+
+
+# def run_game():
+#     agent = BDIAgent()
+#     state = State()
+
+#     while True:
+#         p = agent.get_next_percept()
+#         agent.beliefs = agent.brf(p)
+#         agent.intentions = agent.deliberate()
+#         plan = state.extract_plan(agent.beliefs, agent.intentions)
+#         while not plan == [] and not agent.suceeded() and not agent.impossible():
+#             agent.execute(plan.pop([0]))  # execute first step in plan, and removes step from plan
+#             p = agent.get_next_percept()
+#             agent.beliefs = agent.brf(p)
+#             if agent.reconsider():
+#                 agent.intentions = agent.deliberate()  # Intention reconsideration
+#             if not agent.sound(plan):
+#                 plan = state.extract_plan(agent.beliefs, agent.intentions)
+
+
+# if __name__ == '__main__':
+#     run_game()
