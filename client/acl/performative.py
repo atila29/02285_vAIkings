197e840ffd424270a4a8bf73d974569d7b7fbcc2
Mathias Petersen <mathias.petersen95@gmail.com>
2020-04-20 14:02:14 +0200

    add initial acl

---
+++ client/acl/performative.py
@@ -0,0 +1,54 @@
+# Inquiries and announcements:
+#     1.query-if.  “Is boxBat locationL?” -> might not be necessary because all knowledge is currently available?
+#     2.inform.  “BoxBis at locationL.”
+# Requests:
+#     3.request.  “Please move BoxBto locationL.”
+#     4.agree.  “OK, I’ll do it.”
+#     5.refuse.  “I won’t do it.”
+#     6.failure.  “I didn’t manage to do it.”
+# Proposals:
+#     6.cfp.  Call for proposal.  “Send me your proposals for moving boxBtolocationLwithin timeT.”
+#     7.propose.  “I can do it by ...”
+#     8.accept-proposal.  “OK, I accept your proposal.”
+#     9.reject-proposal.  “No, that’s not good enough.”
+
+# class RequestMoveBoxTo:
+#     def __init__(self, location):
+#         self.location = location
+
+# class RequestMoveSpecificBoxTo:
+#     def __init__(self, boxId, location):
+#         self.boxId = boxId
+#         self.location = location
+
+class CfpMoveBoxTo:
+    def __init__(self, location, cost = None):
+        self.location = location
+
+class CfpMoveSpecificBoxTo:
+    def __init__(self, boxId, location, cost = None):
+        self.boxId = boxId
+        self.location = location
+
+class Propose:
+    def __init__(self, cost):
+        self.cost = cost
+
+class Agree:
+    pass
+
+class Refuse:
+    pass
+
+class Failed:
+    pass
+
+class AcceptProposal:
+    pass
+
+class RejectProposal:
+    pass
+
+class InformProposalDelivered:
+    pass
+
